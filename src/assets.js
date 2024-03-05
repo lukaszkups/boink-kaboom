dff5bad6dee6306029907665e5a06bd2967904ca
lukaszkups <lukaszkups@gmail.com>
2024-03-05 10:03:25 +0100

    Adjusted assets imports.

--- src/assets.js
+++ src/assets.js
@@ -1,11 +1,15 @@
 import k from './kaboom';
 import pixelFont from '/fonts/minimal3x5.ttf';
+import boinkSfx from '/audio/boink.webm';
+import clickSfx from '/audio/click.webm';
+import boostSfx from '/audio/boost.webm';
+import sawSfx from '/audio/saw.webm';
 
 export default function GameAssets () {
-  k.loadSound('boink', '/audio/boink.webm');
-  k.loadSound('click', '/audio/click.webm');
-  k.loadSound('boost', '/audio/boost.webm');
-  k.loadSound('saw', '/audio/saw.webm');
+  k.loadSound('boink', boinkSfx);
+  k.loadSound('click', clickSfx);
+  k.loadSound('boost', boostSfx);
+  k.loadSound('saw', sawSfx);
 
   k.loadFont('pixel', pixelFont);
 }
